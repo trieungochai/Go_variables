a894d22eb63a700b6bc7c96e403c17f99c2eb23c
trieungochai <trieungochai.dev@gmail.com>
2021-10-12 13:52:41 +0700

    Guess the Number Game

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and press ENTER when ready!!!"
+
+func main() {
+	var firstNumber = 2
+	var secondNumber = 5
+	var subtraction = 7
+	// var answer int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// display a welcome/instructions
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
+	fmt.Println("Think of a number between 1 ~ 10", prompt)
+	reader.ReadString('\n')
+
+	// take them through the games
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	// given the answer
+
+}
